15e516fb90cfde903552ef527af6fd4b779bf1cb
convulsivekiddo <106062000+convulsivekiddo@users.noreply.github.com>
2023-04-14 13:23:29 +0300

    Add files via upload

---
+++ foldert/bloc/character_event.dart
@@ -0,0 +1,11 @@
+part of 'character_block.dart';
+
+@freezed
+class CharacterEvent with _$CharacterEvent {
+  const factory CharacterEvent.fetch({
+    required String name,
+    required int page,
+  }) = CharacterEventFetch;
+
+}
+
---
+++ foldert/bloc/character_state.dart
@@ -0,0 +1,10 @@
+part of 'package:rick_and_morty_api/bloc/character_block.dart';
+
+
+@freezed
+class CharacterState with _$CharacterState{
+  const factory CharacterState.loading() = CharacterStateLoading;
+  const factory CharacterState.loaded({required Character characterLoaded}) = CharacterStateLoaded;
+  const factory CharacterState.error() = CharacterStateError;
+}
+
